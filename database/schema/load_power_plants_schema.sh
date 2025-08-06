c8f74ff22ee06abf0ed8f8caf2b2f3e8f50a7fa6
RyanPNSmith <ryanpsmith2003@outlook.com>
2025-08-05 19:05:29 -0500

    Remove redundent scripts, Rename scripts.

--- database/schema/load_power_plants_schema.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# Apply the power_plants schema to the local Postgres database.
-# Requires the database container from docker-compose.yml to be running.
-# Usage: ./load_power_plants_schema.sh
-
-set -e
-psql "postgresql://user:password@localhost:5432/energy" -f "$(dirname "$0")/power_plants.sql"
