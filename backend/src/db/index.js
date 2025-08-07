de8b614e4b2fb9027c5e8da1455b9dd175f25580
RyanPNSmith <ryanpsmith2003@outlook.com>
2025-08-06 22:08:16 -0500

    Add secure API key to project.

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '../.env' });
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 const pool = new Pool({
   host: process.env.DB_HOST,
